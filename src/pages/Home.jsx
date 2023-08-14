0776fd5e664c99d81227d1ccaae835704cede5df
Aleksander Morawski <olek.morawski@gmail.com>
2023-08-14 16:54:12 +0200

    Added sass, normalize and jsx components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,22 @@
+import Nav from "../components/Nav";
+
 const Home = () => {
-  return <div>Home</div>;
+  const authToken = true;
+
+  const handleClick = () => {
+    console.log("clicked");
+  };
+
+  return (
+    <>
+      <Nav />
+      <div className="home">
+        <h1>Have a Party</h1>
+        <button className="btn-primary" onClick={handleClick}>
+          {authToken ? "Singout" : "Create Account"}
+        </button>
+      </div>
+    </>
+  );
 };
 export default Home;
