96fb456e5fc7eebb34aed25499cb4093fd8451d8
Aleksander Morawski <olek.morawski@gmail.com>
2023-08-15 19:12:43 +0200

    imported react-tinder-card lib

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,42 @@
+import { useState } from "react";
+import TinderCard from "react-tinder-card";
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const characters = db;
+  const [lastDirection, setLastDirection] = useState();
+
+  const swiped = (direction, nameToDelete) => {
+    console.log("removing: " + nameToDelete);
+    setLastDirection(direction);
+  };
+
+  const outOfFrame = (name) => {
+    console.log(name + " left the screen!");
+  };
+
+  return (
+    <div className="dashboard">
+      <ChatContainer />
+      <div className="swiper_container">
+        <div className="card_container">
+          {characters.map((character) => (
+            <TinderCard
+              className="swipe"
+              key={character.name}
+              onSwipe={(dir) => swiped(dir, character.name)}
+              onCardLeftScreen={() => outOfFrame(character.name)}
+            >
+              <div
+                style={{ backgroundImage: "url(" + character.url + ")" }}
+                className="card"
+              >
+                <h3>{character.name}</h3>
+              </div>
+            </TinderCard>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Dashboard;
